d6ae2f7e72b495e93f50d355e982d558403fdf0e
ybz2010 <ybz721@126.com>
2024-11-20 19:13:42 +0800

    2024-11-20 T3

--- exam/2024-11-20/grouping.cpp
+++ exam/2024-11-20/grouping.cpp
@@ -5,7 +5,8 @@ using namespace std;
 const int maxn = 1e5 + 5;
 int n,wei;
 int a[maxn],pre[maxn];
-int dp[maxn];
+int dp[maxn],cnt[maxn];
+deque<int>q;
 struct Nahida
 {
     int l,r,min1,max1;
@@ -61,23 +62,39 @@ int cost(int l,int r)
 {
     if (l > r)
         return 0;
-    return pre[r] - pre[l - 1] + querymax(l,r,1) - querymin(l,r,1);
+    return querymax(l,r,1) - querymin(l,r,1);
 }
 signed main()
 {
-    scanf("%lld",&n,&wei);
+    scanf("%lld%lld",&n,&wei);
     for (int i = 1; i <= n; i ++)
     {
         scanf("%lld",a + i);
         pre[i] = pre[i - 1] + a[i];
     }
     build(1,n,1);
-    // for (int i = 1; i <= n; i ++)
-    // {
-    //     dp[i] = inf;
-    //     for (int k = 0; k < i; k ++)
-    //         dp[i] = min(dp[i],dp[k] + cost(k + 1,i) + pre[n] - pre[i]);
-    // }
-    // cout << dp[n];
+    dp[0] = pre[n];
+    q.push_back(0);
+    for (int i = 1; i <= n; i++)
+    {
+        while (!q.empty() && pre[i] - pre[q.front()] > wei)
+            q.pop_front();
+        dp[i] = inf;
+        for (auto j : q)
+        {
+            int x = cost(j + 1,i);
+            if (dp[j] + x + pre[n] - pre[i] < dp[i])
+            {
+                dp[i] = dp[j] + x + pre[n] - pre[i];
+                cnt[i] = cnt[j] + 1;
+            }
+        }
+        while (!q.empty() && dp[i] <= dp[q.back()])
+            q.pop_back();
+        while (!q.empty() && cnt[q.front()] < cnt[i] - 1)
+            q.pop_front();
+        q.push_back(i);
+    }
+    cout << dp[n];
     return 0;
 }
