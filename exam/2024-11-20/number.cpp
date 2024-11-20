3e24e5b59a4d5ac4284193d7c982ce9763a9c73a
ybz2010 <ybz721@126.com>
2024-11-20 15:36:25 +0800

    11月20日 T1

--- exam/2024-11-20/number.cpp
+++ exam/2024-11-20/number.cpp
@@ -1,10 +1,17 @@
 #include<bits/extc++.h>
+#define inf 0x7f7f7f7f7f7f7f7f
 #define int long long
 using namespace std;
 const int maxn = 505;
 int n;
-int a[maxn],pre[maxn];
-int dp[maxn][maxn];
+int a[maxn];
+int dp[maxn][maxn],pre[maxn];
+int sum(int l,int r)
+{
+    if (l > r)
+        return 0;
+    return pre[r] - pre[l - 1];
+}
 signed main()
 {
     scanf("%lld",&n);
@@ -12,18 +19,15 @@ signed main()
     {
         scanf("%lld",a + i);
         pre[i] = pre[i - 1] + a[i];
-        dp[i][i] = 0;
     }
     for (int len = 2; len <= n; len ++)
     {
         for (int l = 1; l + len - 1 <= n; l ++)
         {
             int r = l + len - 1;
+            dp[l][r] = inf;
             for (int i = l; i <= r; i ++)
-            {
-                dp[l][r] = max(dp[l][r],pre[i - 1] - pre[l - 1] + dp[i + 1][r]);
-                dp[l][r] = max(dp[l][r],pre[r] - pre[i] + dp[l][i - 1]);
-            }
+                dp[l][r] = min(dp[l][r],max(sum(l,i - 1) + dp[i + 1][r],sum(i + 1,r) + dp[l][i - 1]));
         }
     }
     cout << dp[1][n];
