7984a89f4ae796b29794f059d1fb75d8aa350260
ybz2010 <ybz721@126.com>
2024-11-24 18:06:04 +0800

    CF2041B

--- contest/codeforces/2041/B.cpp
+++ contest/codeforces/2041/B.cpp
@@ -2,15 +2,16 @@
 #define int long long
 using namespace std;
 int w,b;
+int idx;
 void solve()
 {
 	scanf("%lld%lld",&w,&b);
+	w += b;
 	if (w > b)
 		swap(w,b);
-	while (w)
-	{
-		
-	}
+	idx = 1;
+	printf("%.0lf\n",floor((sqrt(1 + 8 * b) - 1) / 2.0));
+	return;
 }
 signed main()
 {
