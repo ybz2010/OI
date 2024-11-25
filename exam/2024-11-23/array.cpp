39d77ef86165a4754235f696ce2e77c3c2842d4c
ybz2010 <ybz721@126.com>
2024-11-25 15:12:36 +0800

    2024-11-23 T2

--- exam/2024-11-23/array.cpp
+++ exam/2024-11-23/array.cpp
@@ -1,49 +1,48 @@
-#include<bits/extc++.h>
+#include <bits/extc++.h>
 #define int long long
-#define inf 1e18 + 114514
-#define maxn (int)2e7 + 5
+#define inf 1e18 + 1919810
 using namespace std;
-int q,x,y,cnt;
-int ls[maxn],rs[maxn],val[maxn];
-int getr(int c){return c << 1 | 1;}
-int getnum(int x,int y)
+const int maxn = 2e7 + 10;
+int q, x, y, cnt = 1;
+int ls[maxn], rs[maxn], val[maxn];
+int getr(int c) { return c << 1 | 1; }
+int w(int y, int x)
 {
-    int c = max(abs(x),abs(y));
-    int r = getr(c) - 1;
-    int st = pow(getr(c - 1),2);
-    if (y == c && -c + 1 <= x && x <= c)
-        return st + x + c;
-    else if (x == c && c - 1 >= y && y >= -c)
-        return st + r + c - y;
-    else if (y == -c && c - 1 >= x && x >= -c)
-        return st + r * 2 + c - x;
-    else 
-        return st + r * 3 + y + c;
+    if (x == 0 && y == 0)
+        return 1;
+    int p = max(abs(x), abs(y)), res = (2 * p - 1) * (2 * p - 1);
+    if (x == p && y != -p)
+        return res + p + y;
+    if (y == p)
+        return res + 3 * p - x;
+    if (x == -p)
+        return res + 5 * p - y;
+    return res + 7 * p + x;
 }
-int que(int rt,int l,int r,int x)
+int que(int rt, int l, int r, int x)
 {
-    val[rt] --;
+    val[rt]--;
     if (l == r)
         return x;
-    int mid = l + r >> 1;
+    int mid = (l + r) >> 1;
     if (!ls[rt])
-        ls[rt] = ++cnt,val[ls[rt]] = mid - l + 1;
+        ls[rt] = ++cnt, val[ls[rt]] = mid - l + 1;
     if (!rs[rt])
-        rs[rt] = ++cnt,val[rs[rt]] = r - mid;
+        rs[rt] = ++cnt, val[rs[rt]] = r - mid;
     if (val[ls[rt]] >= x)
-        return que(ls[rt],l,mid,x);
+        return que(ls[rt], l, mid, x);
     else
-        return mid - l + 1 + que(rs[rt],mid + 1,r,x - val[ls[rt]]);    
+        return mid - l + 1 + que(rs[rt], mid + 1, r, x - val[ls[rt]]);
 }
 signed main()
 {
-    scanf("%lld",&q);
+    scanf("%lld", &q);
     val[1] = inf;
     while (q--)
     {
-        scanf("%lld%lld",&x,&y);
-        int num = getnum(x,y);
-        printf("%lld\n",que(1,1,inf,num));
+        scanf("%lld%lld", &x, &y);
+        int num = w(x, y);
+        printf("%lld\n", que(1, 1, inf, num));
     }
     return 0;
 }
