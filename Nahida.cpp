7eb0dac28a144654f502662e415cd84e9e7e3283
ybz2010 <ybz721@126.com>
2024-11-30 09:25:45 +0800

    CF2000F

--- Nahida.cpp
+++ Nahida.cpp
@@ -1,51 +1,43 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+ 
 using namespace std;
-#define ll long long
-int b2[5010][5010], b1[5010][5010];
-int n, m, idx;
-// b2[i][j]代表第m-i次决策的r后面大于0的数字j 的个数
-// b2[i][j]代表第m-i次决策的r后面小于0的数字，绝对值j 的个数
-ll dp[5010][5010];
-int main()
-{
-	stack<ll> st;
-	scanf("%d%d", &n, &m);
-	for (int i = 1; i <= n; ++i)
-	{
-		ll l;
-		scanf("%lld", &l);
-		st.push(l);
-	}
-	while (!st.empty())
-	{
-		int r = st.top();
-		st.pop();
-		if (r == 0)
-		{
-			idx++;
-			for (int i = 0; i <= m; ++i)
-			{
-				b2[idx][i] = b2[idx - 1][i];
-				b1[idx][i] = b1[idx - 1][i];
-			}
-		}
-		if (r < 0)
-			b1[idx][-r]++;
-		if (r > 0)
-			b2[idx][r]++;
-	}
-	for (int i = 1; i <= m; ++i)
-	{
-		for (int j = 0; j <= m; ++j)
-		{
-			if (j - 1 >= 0)
-				dp[j][i - j] = max(dp[j][i - j], dp[j - 1][i - j] + b1[m - i][j]);
-			if (i - j - 1 >= 0)
-				dp[j][i - j] = max(dp[j][i - j], dp[j][i - j - 1] + b2[m - i][i - j]);
-		}
-	}
-	ll mxn = 0;
-	for (int i = 0; i <= m; ++i)
-		mxn = max(mxn, dp[i][m - i]);
-	printf("%lld", mxn);
+ 
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i] >> b[i];
+    }
+    vector<vector<int>> dp(n + 1, vector<int>(k + 1, 1e9));
+    dp[0][0] = 0;
+    for (int i = 0; i < n; ++i) {
+        int x = a[i], y = b[i];
+        int xy = x + y;
+        int cost = 0;
+        for (int j = 0; j <= xy; ++j) {
+            for (int j1 = 0; j1 + j <= k; ++j1) {
+                dp[i + 1][j1 + j] = min(dp[i + 1][j1 + j], dp[i][j1] + cost);
+            }
+            if (j < xy) {
+                if (x >= y) {
+                    x--, cost += y;
+                } else {
+                    y--, cost += x;
+                }
+            }
+        }
+    }
+    cout << (dp[n][k] == 1e9 ? -1 : dp[n][k]) << '\n';
+}
+ 
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+        solve();
+    }
+    return 0;
 }
