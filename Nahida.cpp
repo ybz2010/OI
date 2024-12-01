16fabb2e24c524323220734f7c4a6f2239208d67
ybz2010 <ybz721@126.com>
2024-12-01 12:45:46 +0800

    test

--- Nahida.cpp
+++ Nahida.cpp
@@ -1,6 +1,9 @@
-#include <bits/stdc++.h>
+#include<bits/extc++.h>
 using namespace std;
-int main(){
-	char s; int a, b, t; cin >> t;
-	while(t--) scanf("%x%s%x", &a, &s, &b), cout << bitset<13>(a) << ' ' << s << ' ' << bitset<13>(b) << " = " << (s^45?a+b:a-b) << '\n';
+signed main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	cout << a + b;
+	return 0;
 }
