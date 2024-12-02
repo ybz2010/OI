1ad107aaebddddd8e92d00a2a7b36ff6f781a97d
ybz2010 <ybz721@126.com>
2024-12-02 16:00:21 +0800

    P2858

--- Nahida.cpp
+++ Nahida.cpp
@@ -1,44 +1,15 @@
-#include<bits/stdc++.h>
-#define int long long
+#include<cstring>
+#include<cstdio>
+#include<iostream>
 using namespace std;
-int a,b;
-int num[20];
-int f[20][20][5];
-int dfs(int pos,bool lim,int sum,bool zero,int d)
-{
-    if(!pos)
-        return sum;
-    if(!lim&&f[pos][sum][zero])
-        return f[pos][sum][zero];
-    int up=lim?num[pos]:9,ret=0;
-    for(int i=0;i<=up;i++)
-        ret+=dfs(pos-1,lim&&i==up,sum+((!zero||i)&&i==d),zero&&!i,d);
-    f[pos][sum][zero]=ret;
-    return ret;
+char s[700],c;
+int n,l,r,w,ans;
+int main() {
+    scanf("%d%s",&n,s);
+    memcpy(s+n,s,n);
+    for (int i=0; i<n<<1; i++)
+        if (s[i]=='w') w++,r++;
+            else if (s[i]==c) w=0,r++;
+                else ans=max(ans,l+r),l=r-w,r=w+1,w=0,c=s[i];
+    printf("%d",min(max(ans,l+r),n));
 }
-int solve(int x,int d)
-{
-    int idx=0;
-    while(x)
-    {
-        num[++idx]=x%10;
-        x/=10;
-    }
-    memset(f,0,sizeof(f));
-    return dfs(idx,1,0,1,d);
-}
-signed main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    cin>>a>>b;
-    for(int i=0;i<10;i++)
-        cout<<solve(b,i)-solve(a-1,i)<<" ";
-    return 0;
-}
-/*
-Do you play Genshin Impact?
-Genshin Impact,what a waste it is!
-by ybz2010 or __dragon_
-*/
