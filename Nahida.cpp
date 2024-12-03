459beb7a9517b02a7329d588c6ada4d32b9723d0
ybz2010 <ybz721@126.com>
2024-12-03 15:10:07 +0800

    P11361

--- Nahida.cpp
+++ Nahida.cpp
@@ -1,54 +1,96 @@
-#include <iostream>
-#include <cstdio>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
-
-int read()
+const int N = 1e5 + 5;
+char s1[N], s2[N], t1[N], t2[N];
+int n, bel1[N], bel2[N], cnt1[N][2], cnt2[N][2];
+void solve()
 {
-    int f = 1, x = 0;
-    char ss = getchar();
-    while (ss < '0' || ss > '9')
+    cin >> n;
+    cin >> (s1 + 1);
+    cin >> (s2 + 1);
+    cin >> (t1 + 1);
+    cin >> (t2 + 1);
+    int cnt = 0;
+    memset(cnt1, 0, sizeof(cnt1));
+    memset(cnt2, 0, sizeof(cnt2));
+    memset(bel1, 0, sizeof(bel1));
+    memset(bel2, 0, sizeof(bel2));
+    t1[0] = '*';
+    t2[0] = '*';
+    for (int i = 1; i <= n; i++)
     {
-        if (ss == '-')
-            f = -1;
-        ss = getchar();
+        if (t1[i] != t1[i - 1])
+            bel1[i] = ++cnt;
+        else
+            bel1[i] = bel1[i - 1];
+        cnt1[bel1[i]][s1[i] - '0']++;
     }
-    while (ss >= '0' && ss <= '9')
+    cnt = 0;
+    for (int i = 1; i <= n; i++)
     {
-        x = x * 10 + ss - '0';
-        ss = getchar();
+        if (t2[i] != t2[i - 1])
+            bel2[i] = ++cnt;
+        else
+            bel2[i] = bel2[i - 1];
+        cnt2[bel2[i]][s2[i] - '0']++;
     }
-    return f * x;
+    int ans = 0;
+    // 判上下都固定的情况和一边固定的情况
+    for (int i = 1; i <= n; i++)
+    {
+        if (t1[i] == '0' && t2[i] == '0')
+        {
+            if (s1[i] == s2[i])
+                ans++;
+            continue;
+        }
+        else if (t1[i] == '0')
+        {
+            if (cnt2[bel2[i]][s1[i] - '0'])
+            {
+                cnt2[bel2[i]][s1[i] - '0']--;
+                ans++;
+            }
+        }
+        else if (t2[i] == '0')
+        {
+            if (cnt1[bel1[i]][s2[i] - '0'])
+            {
+                cnt1[bel1[i]][s2[i] - '0']--;
+                ans++;
+            }
+        }
+    }
+    // 最后判断上下都不固定的情况
+    for (int i = 1; i <= n; i ++)
+    {
+        if (t1[i] == '1' && t2[i] == '1')
+        {
+            if (cnt1[bel1[i]][0] && cnt2[bel2[i]][0])
+            {
+                cnt1[bel1[i]][0] --;
+                cnt2[bel2[i]][0] --;
+                ans ++;
+            }
+            else if (cnt1[bel1[i]][1] && cnt2[bel2[i]][1])
+            {
+                cnt1[bel1[i]][1] --;
+                cnt2[bel2[i]][1] --;
+                ans ++;
+            }
+        }
+    }
+    cout << ans << '\n';
 }
 
-const int maxn = 1100010;
-int n, ans = 2e9;
-int dp[maxn][25];
-int rem[25][25];
-
-int main()
+signed main()
 {
-    n = read();
-    for (int i = 1; i <= n; ++i)
-        for (int j = 1; j <= n; ++j)
-            rem[i][j] = read();
-
-    memset(dp, 67, sizeof(dp));
-    dp[1][1] = 0; // 状态1表示此时只有1号点访问过
-
-    for (int i = 0; i <= (1 << n) - 1; ++i) // dp过程解释如上
-        for (int j = 1; j <= n; ++j)
-            if (((1 << j - 1) & i) == 0)
-                for (int k = 1; k <= n; ++k)
-                    if (((1 << k - 1) & i))
-                        dp[i | (1 << j - 1)][j] = min(dp[i | (1 << j - 1)][j], dp[i][k] + rem[k][j]);
-
-    for (int i = 2; i <= n; i++) // 最后从状态(1<<n)-1（二进制全为1）中寻找到1最短的点
-        ans = min(ans, dp[(1 << n) - 1][i] + rem[i][1]);
-
-    printf("%lld", ans);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int T;
+    cin >> T;
+    while (T--)
+        solve();
     return 0;
 }
