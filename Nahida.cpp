036b2259a0771a2284b5d13f3c186d17b34946dd
ybz2010 <ybz721@126.com>
2024-12-03 20:17:01 +0800

    CF2042C

--- Nahida.cpp
+++ Nahida.cpp
@@ -1,96 +1,86 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define ll long long
 using namespace std;
-const int N = 1e5 + 5;
-char s1[N], s2[N], t1[N], t2[N];
-int n, bel1[N], bel2[N], cnt1[N][2], cnt2[N][2];
+const int maxv=64+10;
+int t,cnt1,cnt2;
+int x[maxv],z[maxv];
+ll ans,X,Z;
+ll dp[maxv][3][2];
+ll read()
+{
+	ll ret=0,w=1;
+	char ch=0;
+	while(ch<'0'||ch>'9')
+	{
+		if(ch=='-') w=-1;
+		ch=getchar();
+	}
+	while(ch>='0'&&ch<='9')
+	{
+		ret=(ret<<3)+(ret<<1)+(ch^48);
+		ch=getchar();
+	}
+	return ret*w;
+}
+void init()
+{
+	cnt1=cnt2=0;
+	memset(dp,-1,sizeof(dp));
+}
+void inpu() {Z=read(),X=read();}
+void prepa()
+{
+	while(X) {x[++cnt1]=X%10;X/=10;}
+	while(Z) {z[++cnt2]=Z%10;Z/=10;}
+	while(cnt1<cnt2) x[++cnt1]=0;
+	while(cnt2<cnt1) z[++cnt2]=0;
+}
+bool check(int pos,bool legal,int num)
+{
+	if(num!=x[pos]) return num>x[pos];
+	return legal;
+}
+ll dfs(int pos,int isadd,bool islegal)
+{
+	ll ret=0;
+	if(dp[pos][isadd][islegal]!=-1) return dp[pos][isadd][islegal];
+	else if(pos==cnt2+1) return dp[pos][isadd][islegal]=!islegal;
+	else
+	{
+		if(z[pos]==0)
+		{
+			for(int i=0;i<=3;i++) ret+=dfs(pos+1,0,check(pos,islegal,i));
+			if(isadd>=1) ret+=dfs(pos+1,2,check(pos,islegal,4));
+			if(isadd==0) ret+=dfs(pos+1,0,check(pos,islegal,4));
+			for(int i=5;i<=9;i++) ret+=dfs(pos+1,1,check(pos,islegal,i));
+		}else if(z[pos]==1)
+		{
+			if(isadd>=1)
+			{
+				for(int i=0;i<=4;i++) ret+=dfs(pos+1,0,check(pos,islegal,i));
+				for(int i=5;i<=9;i++) ret+=dfs(pos+1,1,check(pos,islegal,i));
+			}else ret+=dfs(pos+1,0,check(pos,islegal,1));
+		}else
+		{
+			if(isadd<=1) ret+=dfs(pos+1,0,check(pos,islegal,z[pos]-isadd));
+			else 
+				for(int i=0;i<=1;i++)
+					ret+=dfs(pos+1,0,check(pos,islegal,z[pos]-i));
+		}	
+	}
+	return dp[pos][isadd][islegal]=ret;
+}
 void solve()
 {
-    cin >> n;
-    cin >> (s1 + 1);
-    cin >> (s2 + 1);
-    cin >> (t1 + 1);
-    cin >> (t2 + 1);
-    int cnt = 0;
-    memset(cnt1, 0, sizeof(cnt1));
-    memset(cnt2, 0, sizeof(cnt2));
-    memset(bel1, 0, sizeof(bel1));
-    memset(bel2, 0, sizeof(bel2));
-    t1[0] = '*';
-    t2[0] = '*';
-    for (int i = 1; i <= n; i++)
-    {
-        if (t1[i] != t1[i - 1])
-            bel1[i] = ++cnt;
-        else
-            bel1[i] = bel1[i - 1];
-        cnt1[bel1[i]][s1[i] - '0']++;
-    }
-    cnt = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        if (t2[i] != t2[i - 1])
-            bel2[i] = ++cnt;
-        else
-            bel2[i] = bel2[i - 1];
-        cnt2[bel2[i]][s2[i] - '0']++;
-    }
-    int ans = 0;
-    // 判上下都固定的情况和一边固定的情况
-    for (int i = 1; i <= n; i++)
-    {
-        if (t1[i] == '0' && t2[i] == '0')
-        {
-            if (s1[i] == s2[i])
-                ans++;
-            continue;
-        }
-        else if (t1[i] == '0')
-        {
-            if (cnt2[bel2[i]][s1[i] - '0'])
-            {
-                cnt2[bel2[i]][s1[i] - '0']--;
-                ans++;
-            }
-        }
-        else if (t2[i] == '0')
-        {
-            if (cnt1[bel1[i]][s2[i] - '0'])
-            {
-                cnt1[bel1[i]][s2[i] - '0']--;
-                ans++;
-            }
-        }
-    }
-    // 最后判断上下都不固定的情况
-    for (int i = 1; i <= n; i ++)
-    {
-        if (t1[i] == '1' && t2[i] == '1')
-        {
-            if (cnt1[bel1[i]][0] && cnt2[bel2[i]][0])
-            {
-                cnt1[bel1[i]][0] --;
-                cnt2[bel2[i]][0] --;
-                ans ++;
-            }
-            else if (cnt1[bel1[i]][1] && cnt2[bel2[i]][1])
-            {
-                cnt1[bel1[i]][1] --;
-                cnt2[bel2[i]][1] --;
-                ans ++;
-            }
-        }
-    }
-    cout << ans << '\n';
+	init();
+	inpu();
+	prepa();
+	ans=dfs(1,0,0);
+	printf("%lld\n",ans);
 }
-
-signed main()
+int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    int T;
-    cin >> T;
-    while (T--)
-        solve();
-    return 0;
+	t=read();
+	while(t--) solve();
+	return 0;
 }
