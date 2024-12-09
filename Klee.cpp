30029b7450aa38162eaf025c848d697e1123ef5b
ybz2010 <ybz721@126.com>
2024-12-09 15:16:23 +0800

    P4342

--- Klee.cpp
+++ Klee.cpp
@@ -1,28 +1,63 @@
 #include <bits/stdc++.h>
-using namespace std;
-int f[2][2010][2010], a[2010];
-int main()
-{
-    int n;
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-        cin >> a[i];
-    for (int i = 1; i <= n; i++)
-        f[0][i][i] = 1;
-    for (int len = 1; len <= n; len++)
-        for (int i = 1, j = i + len; j <= n; i++, j++)
-        {
-            if (a[i] < a[i + 1])
-                f[0][i][j] += f[0][i + 1][j];
-            if (a[i] < a[j])
-                f[0][i][j] += f[1][i + 1][j];
-            if (a[j] > a[j - 1])
-                f[1][i][j] += f[1][i][j - 1];
-            if (a[j] > a[i])
-                f[1][i][j] += f[0][i][j - 1];
-            f[0][i][j] %= 19650827;
-            f[1][i][j] %= 19650827;
-        }
-    cout << (f[0][1][n] + f[1][1][n]) % 19650827;
-    return 0;
+namespace SXL {
+	using std::max;
+	using std::min;
+	using std::cin;
+	using ll = int;
+	constexpr int MAXN = 200;
+	char op[MAXN];
+	int num[MAXN];
+	ll dp[MAXN * 2][MAXN * 2];
+	int g[MAXN * 2][MAXN * 2];
+	void main() {
+		memset(dp,0xc0,sizeof(dp));
+		memset(g,0x3f,sizeof(g));
+		int n;
+		scanf("%d",&n);
+		for(int i = 1;i <= n;i++) {
+			cin >> op[i];
+			scanf("%d",num + i);
+			op[i + n] = op[i];
+			num[i + n] = num[i];
+		}
+		for(int i = 1;i <= 2 * n;i++) {
+			dp[i][i] = num[i];
+			g[i][i] = num[i];
+		}
+		for(int k = 2;k <= n;k++) {
+			for(int i = 1;i + k - 1 <= 2 * n;i++) {
+				int r = i + k - 1;
+				for(int j = i + 1;j <= r;j++) {
+					if(op[j] == 't') {
+						dp[i][r] = max(dp[i][r],dp[i][j - 1] + dp[j][r]);
+						g[i][r] = min(g[i][r],g[i][j - 1] + g[j][r]);
+					} else {
+						dp[i][r] = max(dp[i][r],
+								   max(dp[i][j - 1] * dp[j][r],
+								   max(g[i][j - 1] * g[j][r],
+								   max(dp[i][j - 1] * g[j][r],
+								   	   g[i][j - 1] * dp[j][r]))));
+						g[i][r] = min(dp[i][r],
+								  min(dp[i][j - 1] * dp[j][r],
+								  min(g[i][j - 1] * g[j][r],
+								  min(dp[i][j - 1] * g[j][r],
+								   	   g[i][j - 1] * dp[j][r]))));
+					}
+				}
+			}
+		}
+		ll ans = -0x3f3f3f3f;
+		for(int i = 1;i <= n;i++) {
+			ans = max(ans,dp[i][i + n - 1]);
+		}
+		printf("%d\n",ans);
+		for(int i = 1;i <= n;i++) {
+			if(dp[i][i + n - 1] == ans) printf("%d ",i);
+		}
+		puts("");
+	}
+}
+int main() {
+	SXL::main();
+	return 0;
 }
--- practice/luogu/P4342.cpp
+++ practice/luogu/P4342.cpp
@@ -1,58 +1,56 @@
-#include<bits/extc++.h>
-#define int long long
+#include<iostream>
+#include<algorithm>
+#define inf 0x3f3f3f3f
 using namespace std;
 int n;
 char op[105];
 int a[105];
-int dp[105][105];
+int dp1[105][105],dp2[105][105];
 signed main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
     cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> op[i] >> a[i];
+        op[i + n] = op[i];
+        a[i + n] = a[i]; 
+    }   
     for (int i = 1; i <= (n << 1); i++)
     {
-        if (i & 1)
-            cin >> op[i >> 1];
-        else
-            cin >> a[i >> 1];
-    }
-    for (int i = 1; i <= n; i++)
-        a[i + n] = a[i];
-    for (int i = 0; i < n; i++)
-        op[i + n] = op[i];
-    n <<= 1;
-    for (int i = 1; i <= n; i++)
-        dp[i][i] = a[i];
-    for (int len = 2; len <= n; len++)
-    {
-        for (int i = 1,j = len; j <= n; i++,j++)
+        for (int j = 1; j <= (n << 1); j++)
         {
+            dp1[i][j] = -inf;
+            dp2[i][j] = inf;
+        }
+    }
+    for (int i = 1; i <= (n << 1); i++)
+        dp1[i][i] = dp2[i][i] = a[i];
+    for (int len = 2; len <= n; len ++)
+    {
+        for (int i = 1; i + len - 1 <= (n << 1); i++)
+        {
+            int j = i + len - 1;
             for (int k = i; k < j; k++)
             {
-                if (a[k] == 't')
-                    dp[i][j] = max(dp[i][j],dp[i][k] + dp[k + 1][j]);
+                if (op[k + 1] == 't')
+                {
+                    dp1[i][j] = max(dp1[i][j],dp1[i][k] + dp1[k + 1][j]);
+                    dp2[i][j] = min(dp2[i][j],dp2[i][k] + dp2[k + 1][j]);
+                }
                 else
-                    dp[i][j] = max(dp[i][j],dp[i][k] * dp[k + 1][j]);
+                {
+                    dp1[i][j] = max(dp1[i][j],max(max(dp1[i][k] * dp1[k + 1][j],dp2[i][k] * dp2[k + 1][j]),max(dp1[i][k] * dp2[k + 1][j],dp2[i][k] * dp1[k + 1][j])));
+                    dp2[i][j] = min(dp2[i][j],min(min(dp1[i][k] * dp1[k + 1][j],dp2[i][k] * dp2[k + 1][j]),min(dp1[i][k] * dp2[k + 1][j],dp2[i][k] * dp1[k + 1][j])));
+                }
             }
         }
     }
-    vector<int>ans;
-    int _max = 0;
-    for (int i = 1,j = (n >> 1); j < n; i++,j++)
-    {
-        if (dp[i][j] > _max)
-        {
-            ans.clear();
-            _max = dp[i][j];
-        }
-        if (dp[i][j] == _max)
-            ans.push_back(i);
-    }
-    sort(ans.begin(),ans.end());
+    int _max = -inf;
+    for (int i = 1; i <= n; i++)
+        _max = max(_max,dp1[i][i + n - 1]);
     cout << _max << endl;
-    for (auto i : ans)
-        cout << i << " ";
+    for (int i = 1; i <= n; i++)
+        if (dp1[i][i + n - 1] == _max)
+            cout << i << " ";
     return 0;
 }
