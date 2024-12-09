2ea9a399cb8d49b368dca95051bc34457abb7ce6
ybz2010 <ybz721@126.com>
2024-12-09 21:03:48 +0800

    CF1516D

--- Klee.cpp
+++ Klee.cpp
@@ -1,63 +1,99 @@
-#include <bits/stdc++.h>
-namespace SXL {
-	using std::max;
-	using std::min;
-	using std::cin;
-	using ll = int;
-	constexpr int MAXN = 200;
-	char op[MAXN];
-	int num[MAXN];
-	ll dp[MAXN * 2][MAXN * 2];
-	int g[MAXN * 2][MAXN * 2];
-	void main() {
-		memset(dp,0xc0,sizeof(dp));
-		memset(g,0x3f,sizeof(g));
-		int n;
-		scanf("%d",&n);
-		for(int i = 1;i <= n;i++) {
-			cin >> op[i];
-			scanf("%d",num + i);
-			op[i + n] = op[i];
-			num[i + n] = num[i];
+#include <bits/extc++.h>
+#define int long long
+using namespace std;
+int idx;
+int a[100005], id[100005], pre[100005];
+int ipr[100005], p[100005], skip[100005][25];
+int read()
+{
+	int x = 0, f = 1;
+	char s = getchar();
+	while (s > '9' || s < '0')
+	{
+		if (s == '-')
+			f = -1;
+		s = getchar();
+	}
+	while (s >= '0' && s <= '9')
+	{
+		x = x * 10 + s - '0';
+		s = getchar();
+	}
+	return x * f;
+}
+int ask(int x, int R)
+{
+	int res = 0;
+	for (int i = 20; i >= 0; --i)
+		if (skip[x][i] && skip[x][i] <= R)
+		{
+			res += (1 << i);
+			x = skip[x][i];
 		}
-		for(int i = 1;i <= 2 * n;i++) {
-			dp[i][i] = num[i];
-			g[i][i] = num[i];
+	return res + 1;
+}
+void makePrime(int N = 100000)
+{
+	for (int i = 2; i <= N; ++i)
+	{
+		if (!ipr[i])
+		{
+			p[++idx] = i;
+			id[i] = idx;
 		}
-		for(int k = 2;k <= n;k++) {
-			for(int i = 1;i + k - 1 <= 2 * n;i++) {
-				int r = i + k - 1;
-				for(int j = i + 1;j <= r;j++) {
-					if(op[j] == 't') {
-						dp[i][r] = max(dp[i][r],dp[i][j - 1] + dp[j][r]);
-						g[i][r] = min(g[i][r],g[i][j - 1] + g[j][r]);
-					} else {
-						dp[i][r] = max(dp[i][r],
-								   max(dp[i][j - 1] * dp[j][r],
-								   max(g[i][j - 1] * g[j][r],
-								   max(dp[i][j - 1] * g[j][r],
-								   	   g[i][j - 1] * dp[j][r]))));
-						g[i][r] = min(dp[i][r],
-								  min(dp[i][j - 1] * dp[j][r],
-								  min(g[i][j - 1] * g[j][r],
-								  min(dp[i][j - 1] * g[j][r],
-								   	   g[i][j - 1] * dp[j][r]))));
-					}
-				}
-			}
+		for (int j = 1; j <= idx && i * p[j] <= N; ++j)
+		{
+			ipr[i * p[j]] = 1;
+			if (i % p[j] == 0)
+				break;
 		}
-		ll ans = -0x3f3f3f3f;
-		for(int i = 1;i <= n;i++) {
-			ans = max(ans,dp[i][i + n - 1]);
-		}
-		printf("%d\n",ans);
-		for(int i = 1;i <= n;i++) {
-			if(dp[i][i + n - 1] == ans) printf("%d ",i);
-		}
-		puts("");
 	}
 }
-int main() {
-	SXL::main();
+signed main()
+{
+	makePrime();
+	int n = read(), Q = read();
+	for (int i = 1; i <= n; ++i)
+		a[i] = read();
+	for (int i = 1; i <= idx; ++i)
+		pre[i] = n + 1;
+	for (int j = 0; j <= 20; ++j)
+		skip[n + 1][j] = n + 1;
+	for (int i = n; i >= 1; --i)
+	{
+		int tmp = a[i];
+		int nxt = n + 1;
+		for (int j = 2; j * j <= tmp; ++j)
+		{
+			if (tmp % j == 0)
+			{
+				nxt = min(nxt, pre[id[j]]);
+				while (tmp % j == 0)
+				{
+					tmp /= j;
+				}
+				pre[id[j]] = i;
+			}
+		}
+		if (tmp != 1)
+		{
+			nxt = min(nxt, pre[id[tmp]]);
+			pre[id[tmp]] = i;
+		}
+		// printf("\n");
+		skip[i][0] = min(skip[i + 1][0], nxt);
+	}
+	for (int i = n; i >= 1; --i)
+	{
+		for (int j = 1; j <= 20; ++j)
+		{
+			skip[i][j] = skip[skip[i][j - 1]][j - 1];
+		}
+	}
+	while (Q--)
+	{
+		int l = read(), r = read(), cnt = 0;
+		printf("%lld\n", ask(l, r));
+	}
 	return 0;
 }
