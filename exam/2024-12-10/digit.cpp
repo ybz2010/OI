1469662f63b241f5c22159390e83660380c39386
ybz2010 <ybz721@126.com>
2024-12-10 09:47:59 +0800

    2024-12-10 T1

--- exam/2024-12-10/digit.cpp
+++ exam/2024-12-10/digit.cpp
@@ -1,29 +1,30 @@
 #include<bits/extc++.h>
 #define int long long
 using namespace std;
-const int maxn = 100005;
+const int maxn = 1e5 + 5;
 const int mod = 1e9 + 7;
 char s[maxn];
 int n,d;
-int dp[maxn];
+int dp[2][maxn];
 void solve()
 {
     cin >> (s + 1) >> d;
     n = strlen(s + 1);
     memset(dp,0,sizeof dp);
-    dp[0] = 1;
-    for (int i = 1; i <= n; i++)
+    dp[1][0] = 1;
+    for (int i = 0; i < n; i++)
     {
-        int x = 0,tmp = 0;
-        for (int j = i; j >= 1; j--)
+        int tmp = 0;
+        for (int j = i + 1; j <= n; j++)
         {
-            tmp = (int)(tmp + (s[j] - '0') * pow(10,x++));
-            if (tmp % d == 0)
-                dp[i] = (dp[i] + dp[j - 1]) % mod;
+            tmp = (tmp * 10 + s[j] - '0') % d;
+            if (tmp == 0)
+                dp[1][j] = (dp[1][j] + dp[0][i] + dp[1][i]) % mod;
+            else
+                dp[0][j] = (dp[0][j] + dp[1][i]) % mod;
         }
-        cout << dp[i] << " ";
     }
-    cout << dp[n] << endl;
+    cout << (dp[0][n] + dp[1][n]) % mod << endl;
 }
 signed main()
 {
