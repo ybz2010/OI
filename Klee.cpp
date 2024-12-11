ac6d6ccdfa7d65dfdb7533e2b23da1b835f360ba
ybz2010 <ybz721@126.com>
2024-12-11 11:01:08 +0800

    UVA12983

--- Klee.cpp
+++ Klee.cpp
@@ -1,131 +1,94 @@
 #include <bits/stdc++.h>
+#define int long long
+
 using namespace std;
-#define ll long long
-const int B = 13;
-ll gcd(ll x, ll y) { return x ? gcd(y % x, x) : y; }
-ll pb[B];
-bool leading_zero_needed = 0;
-ll n;
-int b;
-
-namespace subt_small_n
+template <typename T>
+inline void read(T &x)
 {
-
-	bool first;
-	bool u[B];
-	void ss(ll x, int y, bool lz)
+	T res = 0, f = 1;
+	char ch = getchar();
+	while (!isdigit(ch))
 	{
-		if (y == b)
-		{
-			if (x % n == 0)
-			{
-				if (first)
-				{
-					cout << x;
-					exit(0);
-				}
-				else
-					first = 1;
-			}
-			return;
-		}
-		if (leading_zero_needed && y == 0)
-		{
-			ss(0, 1, 1);
-		}
-		else
-		{
-			for (int i = b - 1; i >= 0; --i)
-			{
-				if (!u[i])
-				{
-					if (i || !lz)
-						u[i] = 1;
-					ss(x + i * pb[b - y - 1], y + 1, lz && !i);
-					u[i] = 0;
-				}
-			}
-		}
+		if (ch == '-')
+			f = -1;
+		ch = getchar();
 	}
-
-} // namespace subt_small_n
-
-namespace subt_large_n
-{
-
-	bool first;
-	void ss(ll x, int y, bool lz, int u)
+	while (isdigit(ch))
 	{
-		//	if(x==0)cerr<<"> "<<x<<" "<<y<<" "<<lz<<endl;
-		if (y == b - 5 && n > pb[5] && (x + n - 1) / n * n - x >= pb[5])
-			return;
-		if (y == b - 4 && n > pb[4] && (x + n - 1) / n * n - x >= pb[4])
-			return;
-		//	if(x==2625)cerr<<"! "<<x<<" "<<y<<" "<<lz<<"  "<<u<<endl;
-		if (y == b - 3)
-		{
-			ll z = (x + n - 1) / n * n - x;
-			if (z >= b * b * b)
-				return;
-			ll a[3] = {z / b / b, z / b % b, z % b};
-			//		cerr<<"a "<<a[0]<<" "<<a[1]<<" "<<a[2]<<"  "<<!((u>>a[0])&1)<<" "<<!((u>>a[1])&1)<<" "<<!((u>>a[2])&1)<<" "<<endl;
-			if (a[1] && a[1] == a[2])
-				return;
-			if ((a[0] || !lz) && (a[1] == a[2] || a[2] == a[0] || a[0] == a[1]))
-				return;
-			if ((a[0] || !lz) && !((u >> a[0]) & 1))
-				return;
-			if ((a[0] || a[1] || !lz) && !((u >> a[1]) & 1))
-				return;
-			if ((a[0] || a[1] || a[2] || !lz) && !((u >> a[2]) & 1))
-				return;
-			if (first)
-			{
-				cout << x + z;
-				exit(0);
-			}
-			else
-				first = 1;
-			return;
-		}
-		if (leading_zero_needed && y == 0)
-		{
-			ss(0, 1, 1, (1 << b) - 1);
-		}
-		else
-		{
-			int uu = u;
-			for (; u;)
-			{
-				int i = 31 - __builtin_clz(u);
-				ss(x + i * pb[b - y - 1], y + 1, lz && !i, uu ^ ((i || !lz) << i));
-				u ^= 1 << i;
-			}
-		}
+		res = (res << 1) + (res << 3) + (ch ^ 48);
+		ch = getchar();
 	}
-
+	x = res * f;
 }
 
-int main()
+const int mod = 1e9 + 7;
+int T, n, m, a[1005], dp[1005][1005], apr[1005], tree[1005], len;
+
+int lowbit(int x) { return x & -x; }
+
+void modify(int x, int k)
 {
-	cin >> n >> b;
-	int g = gcd(n, b - 1);
-	int s = b * (b - 1) / 2;
-	if (s % g)
-		leading_zero_needed = 1;
-	if (n % (b * b) == 0)
-		return cout << "-1" << endl, 0;
-	for (int i = pb[0] = 1; i <= b; i++)
-		pb[i] = pb[i - 1] * b;
-	if (n <= b * b * b || b <= 4)
+	while (x <= len)
 	{
-		subt_small_n::ss(0, 0, 1);
-		cout << "-1" << endl;
+		tree[x] = (tree[x] + k) % mod;
+		x += lowbit(x);
 	}
-	else
+}
+
+int query(int x)
+{
+	int ret = 0;
+	while (x > 0)
 	{
-		subt_large_n::ss(0, 0, 1, (1 << b) - 1);
-		cout << "-1" << endl;
+		ret = (ret + tree[x]) % mod;
+		x -= lowbit(x);
 	}
-	exit(0);
+	return ret;
+}
+
+int __lower_bound(int l, int r, int val)
+{
+	while (l <= r)
+	{
+		int mid = l + r >> 1;
+		if (apr[mid] == val)
+			return mid;
+		if (apr[mid] < val)
+			l = mid + 1;
+		else
+			r = mid - 1;
+	}
+	return -1;
+}
+
+signed main()
+{
+	read(T);
+	int Test = 0;
+	while (T--)
+	{
+		read(n), read(m);
+		for (int i = 1; i <= n; i++)
+			read(a[i]), apr[i] = a[i];
+		sort(apr + 1, apr + n + 1);
+		len = unique(apr + 1, apr + n + 1) - (apr + 1);
+		for (int i = 1; i <= n; i++)
+			dp[i][1] = 1;
+		for (int i = 1; i <= n; i++)
+			a[i] = __lower_bound(1, len, a[i]);
+		for (int j = 2; j <= m; j++)
+		{
+			memset(tree, 0, sizeof(tree));
+			for (int i = 1; i <= n; i++)
+			{
+				dp[i][j] = query(a[i] - 1);
+				modify(a[i], dp[i][j - 1]);
+			}
+		}
+		int Answer = 0;
+		for (int i = 1; i <= n; i++)
+			Answer = (Answer + dp[i][m]) % mod;
+		printf("Case #%lld: %lld\n", ++Test, Answer);
+	}
+	return 0;
 }
