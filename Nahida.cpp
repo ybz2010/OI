15f7cdf7723eab2b9073ce0cd91c69c92738f1e8
ybz2010 <ybz721@126.com>
2024-12-11 19:52:56 +0800

    P10977

--- Nahida.cpp
+++ Nahida.cpp
@@ -1,60 +1,135 @@
-#include <bits/stdc++.h>
-using namespace std;
-const int mod = 1e9 + 7;
-char s[100005];
-int n, d, p, D, hsh[100005], cnt2, cnt5, t;
-int f1[100005], f2[100005], sm1[1000005], sm2[1000005];
-void work()
+#include <cstdio>
+#include <ctype.h>
+#include <cstring>
+#include <algorithm>
+#define ll long long
+#define ls (now << 1)
+#define rs (now << 1 | 1)
+const int kMaxn = 1e5 + 10;
+const ll kInf = 1e12 + 2077;
+struct SegmentTree
 {
-    scanf("%s%d", s + 1, &d);
-    n = strlen(s + 1);
-    cnt2 = cnt5 = 0;
-    D = d;
-    p = 1;
-    while (d % 2 == 0)
-        d /= 2, p *= 2, cnt2++;
-    while (d % 5 == 0)
-        d /= 5, p *= 5, cnt5++;
-    t = max(cnt2, cnt5);
-    for (int i = n, pw = 1; i > 0; i--, pw = pw * 10 % d)
-        hsh[i] = (hsh[i + 1] + (s[i] - '0') * pw) % d;
-    f1[0] = 1;
-    f2[0] = 0;
-    sm1[hsh[1]] = !t;
-    for (int i = 1, sum = 1; i <= n; i++)
-    {
-        f2[i] = sum;
-        int v = 0;
-        for (int j = 0, pw = 1; j < i && j < t; j++, pw = 10 * pw % D)
-        {
-            v = (v + pw * (s[i - j] - '0')) % D;
-            if (!v)
-            {
-                f1[i] = (f1[i] + f2[i - j - 1]) % mod;
-                f1[i] = (f1[i] + f1[i - j - 1]) % mod;
-                f2[i] = (f2[i] + mod - f1[i - j - 1]) % mod;
-            }
-        }
-        if (v % p == 0)
-        {
-            f1[i] = (f1[i] + sm2[hsh[i + 1]]) % mod;
-            f1[i] = (f1[i] + sm1[hsh[i + 1]]) % mod;
-            f2[i] = (f2[i] + mod - sm1[hsh[i + 1]]) % mod;
-        }
-        sum = (sum + f1[i]) % mod;
-        if (i >= t)
-        {
-            sm1[hsh[i - t + 1]] = (sm1[hsh[i - t + 1]] + f1[i - t]) % mod;
-            sm2[hsh[i - t + 1]] = (sm2[hsh[i - t + 1]] + f2[i - t]) % mod;
-        }
-    }
-    printf("%d\n", (f1[n] + f2[n]) % mod);
-    for (int i = 1; i <= n + 1; i++)
-        sm1[hsh[i]] = sm2[hsh[i]] = 0, hsh[i] = f1[i] = f2[i] = 0;
+    int L, R;
+    ll f, ans, lazy;
+} tree[kMaxn << 2];
+ll n, m, h[kMaxn], sum[kMaxn], pre[kMaxn], f[kMaxn];
+ll top, sta[kMaxn];
+inline ll read()
+{
+    ll f = 1, w = 0;
+    char ch = getchar();
+    for (; !isdigit(ch); ch = getchar())
+        if (ch == '-')
+            f = -1;
+    for (; isdigit(ch); ch = getchar())
+        w = (w << 3) + (w << 1) + (ch ^ '0');
+    return f * w;
 }
+void Pushup(int now)
+{
+    tree[now].f = std ::min(tree[ls].f, tree[rs].f);
+    tree[now].ans = std ::min(tree[ls].ans, tree[rs].ans);
+}
+void Pushdown(int now)
+{
+    tree[ls].ans = tree[ls].f + tree[now].lazy;
+    tree[rs].ans = tree[rs].f + tree[now].lazy;
+    tree[ls].lazy = tree[rs].lazy = tree[now].lazy;
+    tree[now].lazy = kInf;
+}
+void Build(int now, int L, int R)
+{
+    tree[now].L = L, tree[now].R = R;
+    if (L == R)
+    {
+        tree[now].f = tree[now].ans = tree[now].lazy = kInf;
+        return;
+    }
+    int mid = (L + R) >> 1;
+    Build(ls, L, mid), Build(rs, mid + 1, R);
+    Pushup(now);
+}
+ll que(int now, int L, int R)
+{
+    if (L <= tree[now].L && tree[now].R <= R)
+        return tree[now].ans;
+    if (tree[now].lazy != kInf)
+        Pushdown(now);
+    int mid = (tree[now].L + tree[now].R) >> 1;
+    ll ret = kInf;
+    if (L <= mid)
+        ret = std ::min(ret, que(ls, L, R));
+    if (R > mid)
+        ret = std ::min(ret, que(rs, L, R));
+    return ret;
+}
+void upd1(int now, int L, int R, ll val)
+{
+    if (L <= tree[now].L && tree[now].R <= R)
+    {
+        tree[now].ans = tree[now].f + val;
+        tree[now].lazy = val;
+        return;
+    }
+    if (tree[now].lazy != kInf)
+        Pushdown(now);
+    int mid = (tree[now].L + tree[now].R) >> 1;
+    if (L <= mid)
+        upd1(ls, L, R, val);
+    if (R > mid)
+        upd1(rs, L, R, val);
+    Pushup(now);
+}
+void upd2(int now, int pos)
+{
+    if (tree[now].L == tree[now].R)
+    {
+        tree[now].ans = kInf;
+        tree[now].f = f[pos - 1];
+        return;
+    }
+    if (tree[now].lazy != kInf)
+        Pushdown(now);
+    int mid = (tree[now].L + tree[now].R) >> 1;
+    if (pos <= mid)
+        upd2(ls, pos);
+    else
+        upd2(rs, pos);
+    Pushup(now);
+}
+void init()
+{
+    n = read(), m = read();
+    for (int i = 1; i <= n; ++i)
+    {
+        h[i] = read();
+        sum[i] = sum[i - 1] + h[i];
+    }
+
+    sta[++top] = 1;
+    for (int i = 2; i <= n; ++i)
+    {
+        while (top && h[sta[top]] < h[i])
+            top--;
+        if (top)
+            pre[i] = sta[top];
+        sta[++top] = i;
+    }
+    Build(1, 1, n);
+}
+//=============================================================
 int main()
 {
-    int tasd;
-    for (scanf("%d", &tasd), 1; tasd--;)
-        work();
+    init();
+    for (int i = 1; i <= n; ++i)
+    {
+        upd2(1, i);
+        if (pre[i] < i)
+            upd1(1, pre[i] + 1, i, h[i]);
+        int l = std ::lower_bound(sum, sum + i + 1, sum[i] - m) - sum;
+        if (l < i)
+            f[i] = que(1, l + 1, i);
+    }
+    printf("%lld", f[n]);
+    return 0;
 }
