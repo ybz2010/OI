0327bf996770405ea2ee72df36885d504b47d807
ybz2010 <ybz721@126.com>
2024-12-12 15:31:43 +0800

    2024-12-12 T2

--- Klee.cpp
+++ Klee.cpp
@@ -1,23 +1,36 @@
-#include<bits/extc++.h>
+#include <bits/stdc++.h>
+// #define int long long
+
 using namespace std;
-struct node
-{
-	int val;
-	node *nxt;
-};
-node *head = NULL;
-void push(int x)
-{
-	node *tmp = new node;
-	tmp->val = x;
-	tmp->nxt = head;
-	head = tmp;
+template <typename T>
+inline void read(T &x){
+    T res = 0,f = 1;
+    char ch = getchar();
+    while(!isdigit(ch)){if(ch == '-') f = -1;ch = getchar();}
+    while(isdigit(ch)){res = (res << 1) + (res << 3) + (ch ^ 48);ch = getchar();}
+    x = res * f;
 }
-signed main()
-{
-	basic_string<int>v1 = {114514,1919810};
-	v1 += 114;
-	for (auto i : v1)
-		cout << i;
-	return 0;
+
+const int maxn = 2e6 + 10;
+int n,m,p[maxn],apr[maxn];
+bool flag[maxn];
+
+signed main(){
+    read(n),read(m);
+    int Ta = max(n,m),Tb = sqrt(Ta) + 1;
+    for(int i = 1;i <= Ta;i ++) apr[i] = i;
+    flag[0] = flag[1] = 1;
+    for(int i = 2;i <= Tb;i ++){
+        if(!flag[i]){
+            for(int j = i;j <= Ta;j += i){
+                flag[j] = 1;
+                while(!(apr[j] % (i * i))) apr[j] /= (i * i);
+            }
+        }
+    }
+    int Answer = 0;
+    for(int i = 1;i <= n;i ++)
+        Answer += floor(sqrt(m / apr[i]));
+    printf("%d\n",Answer);
+    return 0;
 }
--- exam/2024-12-12/B/one.cpp
+++ exam/2024-12-12/B/one.cpp
@@ -1,23 +1,40 @@
 #include<bits/extc++.h>
-#define eps 1e-8
 using namespace std;
+constexpr int maxn(2e6 + 5);
 int n,m;
+int val[maxn];
+bool b[maxn];
+void read(int &x)
+{
+    x = 0;
+    int f = 1;
+    char ch = getchar();
+    while (!isdigit(ch)){f = ch == '-' ? -1 : 1; ch = getchar();}
+    while (isdigit(ch)){x = x * 10 + ch - '0'; ch = getchar();}
+    x *= f;
+}
 signed main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    cin >> n >> m;
-    int cnt = 0;
-    for (int i = 1; i <= n; i++)
+    read(n),read(m);
+    int max1(max(n,m));
+    for (int i(1); i <= max1; i++)
+        val[i] = i;
+    b[0] = b[1] = 1;
+    for (int i(2); i * i <= max1; i++)
     {
-        for (int j = 1; j <= m; j++)
+        if (!b[i])
         {
-            int tmp = floor(sqrt(1.0 * i * j));
-            if (abs(tmp * tmp - i * j) < eps)
-                cnt ++;
+            for (int j(i); j <= max1; j += i)
+            {
+                b[j] = 1;
+                while (val[j] % (i * i) == 0)
+                    val[j] /= (i * i);
+            }
         }
     }
-    cout << cnt;
+    int ans(0);
+    for (int i(1); i <= n; i++)
+        ans += floor(sqrt(1.0 * m / val[i]));
+    printf("%d",ans);
     return 0;
 }
