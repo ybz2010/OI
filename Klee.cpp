cd810ffac3a301d748a1e68b9bc5adfae6120ca1
ybz2010 <ybz721@126.com>
2024-12-13 11:47:23 +0800

    P3188

--- Klee.cpp
+++ Klee.cpp
@@ -1,84 +1,56 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define il inline
+#define int long long //不开ll见祖宗 
+
 using namespace std;
-int read()
+const int N = 1e6 + 10;
+
+vector<int> v[N], b[N];
+
+il int re()
 {
-    int s = 0, w = 1;
-    char ch = getchar();
-    while (ch < '0' || ch > '9')
-    {
-        if (ch == '-')
-            w = -1;
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9')
-        s = (s << 3) + (s << 1) + (ch ^ 48), ch = getchar();
-    return s * w;
+	int x = 0, p = 1;
+	char ch = getchar();
+	while(ch < '0' || ch > '9') {if(ch == '-') p = -1; ch = getchar();}
+	while(ch <= '9' && ch >= '0') {x = (x << 3) + (x << 1) + (ch ^ 48); ch = getchar();}
+	return x * p;
 }
-const int maxn = 3010;
-vector<int> g2[maxn], g1[maxn], g3[maxn];//g是反图，z是正图，G是树
-char vis[maxn];
-int st[maxn], ans[400010], n, m, q;
-void add(int x, int y) { g2[y].push_back(x), g1[x].push_back(y); }
-void ADD(int x, int y) { g3[x].push_back(y); }
-void dfs1(int u)
+
+int n, m, s;
+int dp2[50][5000], dp1[50][5000];//注意这里不要开太小！！！ 
+
+void init()//多组数据 
 {
-    vis[u] = 1;
-    for (auto v : g2[u])
-        if (!vis[v])
-            dfs1(v);
+	s = 0;
+    memset(dp1,0,sizeof dp1);
+    memset(dp2,0,sizeof dp2);;
+	for(int i = 0; i <= 50; ++i) v[i].clear(), b[i].clear();
 }
-struct query{int u,v,k,id;};
-vector<query> q1[maxn], q2[maxn];
-void dfs2(int u)
-{
-    st[++st[0]] = u;
-    for (auto i : q2[u])
-        if (st[0] >= i.k)
-            ans[i.id] = st[st[0] - i.k + 1];
-    for (auto v : g3[u])
-        dfs2(v);
-    st[0] --;
-}
+
 signed main()
 {
-    cin >> n >> m >> q;
-    int u,v,k;
-    for (int i = 1; i <= m; i++)
-    {
-        cin >> u >> v;
-        g1[u].push_back(v);
-        g2[v].push_back(u);
-    }
-    for (int i = 1; i <= n; i++)
-        sort(g1[i].begin(),g1[i].end());
-    for (int i = 1; i <= q; i++)
-    {
-        cin >> u >> v >> k;
-        q1[v].push_back((query){u,v,k,i});
-    }
-    memset(ans,0xff,sizeof ans);
-    for (int ed = 1; ed <= n; ed++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            vis[j] = 0;
-            q2[j].clear();
-            g3[j].clear();
-        }
-        dfs1(ed);
-        for (int i = 1; i <= n; i++)
-            if (vis[i] && i != ed)
-                for (auto v : g1[i])
-                    if (vis[v])
-                    {
-                        ADD(v, i);
-                        break;
-                    }
-        for (auto y : q1[ed])
-            q2[y.u].push_back(y);
-        dfs2(ed);
-    }
-    for (int i = 1; i <= q; i++)
-        cout << ans[i] << '\n';
-    return 0;
+	while(1)
+	{
+		n = re(), m = re();
+		if(n == -1) break;
+		init();
+    	int u,val,tmp;
+		for(int i = 1; i <= n; ++i)//读入
+		{
+			u = re(), val = re(), tmp = 0;
+			while(((u >> tmp) & 1) == 0) tmp++;//记录每一个物品的二进制的表示 
+			v[tmp].push_back(val);
+			b[tmp].push_back((u >> tmp));
+		}
+		while((m >> s)) s++;//总的体积的位数 
+		for (int i = 0; i <= s; i++)
+			for (int j = 0; j < b[i].size(); j++)
+				for (int k = 500; k >= b[i][j]; k--)
+					dp1[i][k] = max(dp1[i][k],dp1[i][k - b[i][j]] + v[i][j]);
+		for (int i = 0; i <= s; i++)
+			for (int j = 500; j >= 0; j--)
+				for (int k = 0; k <= j; k++)
+					dp2[i][j] = max(dp2[i][j],dp2[i - 1][((j - k) << 1) + ((m >> (i - 1)) & 1)] + dp1[i][k]);
+		printf("%lld\n", dp2[s - 1][1]);
+	}
 }
