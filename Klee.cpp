ad5c333d11d34c70259b38f337a4870859ebae34
ybz2010 <ybz721@126.com>
2024-12-13 20:53:09 +0800

    P4182

--- Klee.cpp
+++ Klee.cpp
@@ -1,56 +1,13 @@
-#include<bits/stdc++.h>
-#define il inline
-#define int long long //不开ll见祖宗 
-
+#include<bits/extc++.h>
 using namespace std;
-const int N = 1e6 + 10;
-
-vector<int> v[N], b[N];
-
-il int re()
-{
-	int x = 0, p = 1;
-	char ch = getchar();
-	while(ch < '0' || ch > '9') {if(ch == '-') p = -1; ch = getchar();}
-	while(ch <= '9' && ch >= '0') {x = (x << 3) + (x << 1) + (ch ^ 48); ch = getchar();}
-	return x * p;
-}
-
-int n, m, s;
-int dp2[50][5000], dp1[50][5000];//注意这里不要开太小！！！ 
-
-void init()//多组数据 
-{
-	s = 0;
-    memset(dp1,0,sizeof dp1);
-    memset(dp2,0,sizeof dp2);;
-	for(int i = 0; i <= 50; ++i) v[i].clear(), b[i].clear();
-}
-
 signed main()
 {
-	while(1)
+	int t = 10;
+	while (t --)
 	{
-		n = re(), m = re();
-		if(n == -1) break;
-		init();
-    	int u,val,tmp;
-		for(int i = 1; i <= n; ++i)//读入
-		{
-			u = re(), val = re(), tmp = 0;
-			while(((u >> tmp) & 1) == 0) tmp++;//记录每一个物品的二进制的表示 
-			v[tmp].push_back(val);
-			b[tmp].push_back((u >> tmp));
-		}
-		while((m >> s)) s++;//总的体积的位数 
-		for (int i = 0; i <= s; i++)
-			for (int j = 0; j < b[i].size(); j++)
-				for (int k = 500; k >= b[i][j]; k--)
-					dp1[i][k] = max(dp1[i][k],dp1[i][k - b[i][j]] + v[i][j]);
-		for (int i = 0; i <= s; i++)
-			for (int j = 500; j >= 0; j--)
-				for (int k = 0; k <= j; k++)
-					dp2[i][j] = max(dp2[i][j],dp2[i - 1][((j - k) << 1) + ((m >> (i - 1)) & 1)] + dp1[i][k]);
-		printf("%lld\n", dp2[s - 1][1]);
+		static int n = 0;
+		n ++;
+		cout << n << endl;
 	}
+	return 0;
 }
--- Nahida.cpp
+++ Nahida.cpp
@@ -1,177 +1,62 @@
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
-#define sL (s << 1)
-#define sR (s << 1 | 1)
-
+#include <bits/extc++.h>
+#define first l
+#define second r
 using namespace std;
-const int Maxn = 0x3f3f3f3f;
-const int N = 2e4 + 5, M = N << 2;
-int d[N], c[N], w[N], s[N], st[N], ed[N], f[N];
-int n, k, Ans, val[M], tag[M];
-
-struct point
+int n, m;
+const int N = 1e5 + 5;
+struct Node
 {
-    int to;
-    point *nxt;
-} a[M], *T = a, *lst[N];
-
-inline void addEdge(const int &x, const int &y)
+    int l, r;
+} a[N], b[N];
+int dp[N][110], max1[N];
+deque<int> q[N];
+bool cmp(Node x, Node y)
 {
-    T->nxt = lst[x];
-    T->to = y;
-    lst[x] = T++;
+    return x.l ^ y.l ? x.l < y.l : x.r < y.r;
 }
-template <class T>
-inline T Min(const T &a, const T &b) { return a < b ? a : b; }
-template <class T>
-inline void CkMin(T &a, const T &b)
-{
-    if (a > b)
-        a = b;
-}
-
-inline int get()
-{
-    char ch;
-    bool f = false;
-    int res = 0;
-    while (((ch = getchar()) < '0' || ch > '9') && ch != '-')
-        ;
-    if (ch == '-')
-        f = true;
-    else
-        res = ch - '0';
-    while ((ch = getchar()) >= '0' && ch <= '9')
-        res = (res << 3) + (res << 1) + ch - '0';
-    return f ? ~res + 1 : res;
-}
-
-inline void put(int x)
-{
-    if (x < 0)
-        x = ~x + 1, putchar('-');
-    if (x > 9)
-        put(x / 10);
-    putchar(x % 10 + 48);
-}
-
-inline void Push(const int &s) { val[s] = Min(val[sL], val[sR]); }
-inline void Add(const int &s, const int &z)
-{
-    val[s] += z;
-    tag[s] += z;
-}
-
-inline void Down(const int &s)
-{
-    if (!tag[s])
-        return;
-    Add(sL, tag[s]);
-    Add(sR, tag[s]);
-    tag[s] = 0;
-}
-
-inline void Build(const int &s, const int &l, const int &r)
-{
-    tag[s] = 0;
-    if (l == r)
-        return (void)(val[s] = f[l]);
-    int mid = l + r >> 1;
-    Build(sL, l, mid);
-    Build(sR, mid + 1, r);
-    Push(s);
-}
-
-inline int Query(const int &s, const int &l, const int &r, const int &x, const int &y)
-{
-    if (l == x && r == y)
-        return val[s];
-    Down(s);
-    int mid = l + r >> 1;
-    if (y <= mid)
-        return Query(sL, l, mid, x, y);
-    else if (x > mid)
-        return Query(sR, mid + 1, r, x, y);
-    else
-        return Min(Query(sL, l, mid, x, mid),Query(sR, mid + 1, r, mid + 1, y));
-}
-
-inline void Modify(const int &s, const int &l, const int &r, const int &x, const int &y, const int &z)
-{
-    if (l == x && r == y)
-        return Add(s, z);
-    Down(s);
-    int mid = l + r >> 1;
-    if (y <= mid)
-        Modify(sL, l, mid, x, y, z);
-    else if (x > mid)
-        Modify(sR, mid + 1, r, x, y, z);
-    else
-    {
-        Modify(sL, l, mid, x, mid, z);
-        Modify(sR, mid + 1, r, mid + 1, y, z);
-    }
-    Push(s);
-}
-
 int main()
 {
-    n = get();
-    k = get() + 1;
-    for (int i = 2; i <= n; ++i)
-        d[i] = get();
-    for (int i = 1; i <= n; ++i)
-        c[i] = get();
-    for (int i = 1; i <= n; ++i)
-        s[i] = get();
-    for (int i = 1; i <= n; ++i)
-        w[i] = get();
-    ++n;
-    d[n] = w[n] = Maxn;
-    // 当我们推导i时，我们只考虑了它和前面的基站产生的影响
-    // 这时对于最后一个基站我们不会考虑它和之后的村庄产生的影响
-    // 则我们可以在最后增加一个村庄
-    // 保证它必定被作为基站（无建设费用）且不对前面产生影响
-    // 这样就不会有遗漏的了
-    for (int i = 1; i <= n; ++i)
-    {
-        st[i] = lower_bound(d + 1, d + n + 1, d[i] - s[i]) - d;
-        ed[i] = lower_bound(d + 1, d + n + 1, d[i] + s[i]) - d;
-        if (d[ed[i]] > d[i] + s[i])
-            ed[i]--;
-        addEdge(ed[i], i);
-        // lower_bound查找的是大于等于x的第一个数
-        // 而ed[i]要求的是小于等于x的最后一个数
-        // 所以判断一下减一就可以了
-    }
+    scanf("%d%d", &n, &m);
     for (int i = 1; i <= n; i++)
-        cout << st[i] << " " << ed[i] << endl;
-    for (int j = 1; j <= k; ++j)
-        if (j == 1)
-        {
-            int res = 0;
-            for (int j = 1; j <= n; ++j)
-            {
-                f[j] = res + c[j];
-                for (point *e = lst[j]; e; e = e->nxt)
-                    res += w[e->to];
-            }
-            Ans = f[n];
-        }
+        scanf("%d%d", &a[i].l, &a[i].r);
+    if (m >= n)
+    {
+        puts("0");
+        return 0;
+    }
+    sort(a + 1, a + n + 1, cmp);
+    int mx = -1, cnt = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (a[i].r > mx)
+            b[++cnt] = a[i];
         else
+            m--;
+        mx = max(mx, a[i].r);
+    }
+    m = max(m, 0);
+    b[++cnt].l = 1e9, b[cnt].r = 1e9;
+    n = cnt;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= min(i - 1, m); j++)
         {
-            Build(1, 1, n);
-            int y;
-            for (int i = 1; i <= n; ++i)
+            int pos = i - j - 1;
+            while (q[pos].size() && b[q[pos].front()].r < b[i].l)
             {
-                f[i] = (i >= j ? Query(1, 1, n, j - 1, i - 1) : 0) + c[i];
-                for (point *e = lst[i]; e; e = e->nxt)
-                    if (st[y = e->to] > 1)
-                        Modify(1, 1, n, 1, st[y] - 1, w[y]);
+                max1[pos] = max(max1[pos], dp[q[pos].front()][q[pos].front() - pos]);
+                q[pos].pop_front();
             }
-            CkMin(Ans, f[n]);
+            dp[i][j] = max(dp[i][j],max1[pos] + b[i].second - b[i].first);
+            if (!q[pos].empty())
+                dp[i][j] = max(dp[i][j],dp[q[pos].front()][q[pos].front() - pos] + b[i].second - b[q[pos].front()].second);
+            pos = i - j;
+            while (!q[pos].empty() && dp[q[pos].back()][q[pos].back() - pos] - b[q[pos].back()].second <= dp[i][j] - b[i].second)
+                q[pos].pop_back();
+            q[pos].push_back(i);
         }
-    return put(Ans), 0;
+    }
+    printf("%d", dp[n][m]);
+    return 0;
 }
+//nohtyp
