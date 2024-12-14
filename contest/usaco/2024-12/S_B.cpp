cd2a06a5095b8318861fb11e63265cc0f4befb89
ybz2010 <ybz721@126.com>
2024-12-14 11:46:54 +0800

    usaco 2024-12 S_B

--- contest/usaco/2024-12/S_B.cpp
+++ contest/usaco/2024-12/S_B.cpp
@@ -1,43 +1,49 @@
-#include<bits/extc++.h>
-#define inf 0x7f7f7f7f7f7f
+#include <bits/extc++.h>
 #define int long long
+#define inf 0x3f3f3f3f3f3f3f3f
 using namespace std;
 using namespace __gnu_pbds;
-const int maxn = 1e5 + 5;
-int n,m;
-int l[maxn],r[maxn],w[maxn];
-int a[maxn],dis[maxn];
-bool vis[maxn];
-vector<int>x;
-gp_hash_table<int,int>id;
+const int maxn = 500005;
 struct edge
 {
-    int v,w;
-    edge *nxt;
-}*head[maxn << 1];
-void adde(int u,int v,int w)
+    int v, w, nxt;
+} e[maxn];
+int n, m, a[maxn];
+int u[maxn], v[maxn], w[maxn];
+int head[maxn], idx;
+bool vis[maxn];
+int dis[maxn];
+void init()
 {
-    auto tmp = new edge;
-    tmp->v = v;
-    tmp->w = w;
-    tmp->nxt = head[u];
-    head[u] = tmp;
+    for (int i = 1; i <= 1e5; i++)
+        e[i].v = e[i].w = e[i].nxt = 0;
+    idx = 0;
+    memset(head, 0, sizeof head);
+}
+void adde(int u, int v, int w)
+{
+    idx++;
+    e[idx].v = v;
+    e[idx].w = w;
+    e[idx].nxt = head[u];
+    head[u] = idx;
 }
 void spfa(int st)
 {
-    memset(dis,~0x3f,sizeof dis);
-    memset(vis,0,sizeof vis);
-    queue<int>q;
+    memset(dis, -0x3f, sizeof dis);
+    memset(vis, 0, sizeof vis);
+    queue<int> q;
     dis[st] = 0;
+    vis[st] = 1;
     q.push(st);
     while (!q.empty())
     {
         int u = q.front();
         q.pop();
         vis[u] = 0;
-        for (auto i = head[u]; i; i = i->nxt)
+        for (int i = head[u]; i; i = e[i].nxt)
         {
-            int v = i->v,w = i->w;
+            int v = e[i].v, w = e[i].w;
             if (dis[v] < dis[u] + w)
             {
                 dis[v] = dis[u] + w;
@@ -48,54 +54,37 @@ void spfa(int st)
                 }
             }
         }
-    }   
-}
-void mian()
-{
-    cin >> n >> m;
-    x.clear();
-    memset(head,0,sizeof head);
-    for (int i = 1; i <= n; i++)
-        cin >> a[i];
-    sort(a + 1,a + n + 1);
-    x.push_back(-inf);
-    x.push_back(inf);
-    for (int i = 1; i <= m; i++)
-    {
-        cin >> l[i] >> r[i] >> w[i];
-        l[i] --;
-        x.push_back(l[i]);
-        x.push_back(r[i]);
     }
-    sort(x.begin(),x.end());
-    unique(x.begin(),x.end());
-    for (int i = 1; i < x.size(); i++)
-        id[x[i]] = i;
+}
+void solve()
+{
+    init();
+    scanf("%lld%lld", &n, &m);
+    for (int i = 1; i <= n; i++)
+        scanf("%lld", a + i);
+    sort(a + 1, a + n + 1);
     for (int i = 1; i <= m; i++)
-        adde(id[l[i]],id[r[i]],w[i]);
-    for (int i = 1; i < x.size(); i++)
     {
-        int tl = upper_bound(a + 1,a + n + 1,x[i - 1]) - a;
-        int tr = upper_bound(a + 1,a + n + 1,x[i]) - a - 1;
-        adde(i,i - 1,tl - tr - 1);
-        adde(i - 1,i,0);
+        scanf("%lld%lld%lld", &u[i], &v[i], &w[i]);
+        u[i] = lower_bound(a + 1, a + n + 1, u[i]) - a - 1;
+        v[i] = upper_bound(a + 1, a + n + 1, v[i]) - a - 1;
+        adde(u[i], v[i], w[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        adde(i, i + 1, 0);
+        adde(i + 1, i, -1);
     }
     spfa(0);
-    cout << n - dis[x.size() - 1] << endl;
+    cout << n - dis[n] << "\n";
 }
 signed main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
     int t;
-    cin >> t;
+    scanf("%lld", &t);
     while (t--)
-        mian();
+    {
+        solve();
+    }
     return 0;
 }
-//dis[r] - dis[l - 1] >= w;
-//dis[r] >= dis[l - 1] + w;
-//dis[i] >= dis[i - 1]
-//dis[r] <= dis[l - 1] + w;
-//dis[l - 1] >= dis[r] - w;
