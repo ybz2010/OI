fc5184125ef729293502de6713254d8c1109713d
ybz2010 <ybz721@126.com>
2024-12-17 19:39:00 +0800

    P5059

--- practice/luogu/P5059.cpp
+++ practice/luogu/P5059.cpp
@@ -1,73 +1,60 @@
-#include<bits/extc++.h>
-#define int __int128
+#include <bits/extc++.h>
+#define int long long
 using namespace std;
-int n,mod = 114514;
-void read(int &x)
+int n,mod,temp[2][2];
+int mul(int a,int b)
 {
-    x = 0;
-    short f = 1;
-    char ch = getchar();
-    while (!isdigit(ch)){f = ch == '-' ? -1 : 1; ch = getchar();}
-    while (isdigit(ch)){x = x * 10 + ch - '0'; ch = getchar();}
-    x *= f;
-}
-void write(int x)
-{
-    if (x > 10)
-        write(x / 10);
-    putchar('0' + x % 10);
-}
-struct modint
-{
-    int val;
-    modint(int x = 0):val(x % mod){};
-    int &operator()(){return (val %= mod);}
-    friend modint operator+(modint x,modint y){return ((x() + y()) % mod);}
-    friend modint operator+=(modint &x,modint y){return (x() += y()) %= mod;}
-    friend modint operator-(modint x,modint y){return (((x() - y()) % mod + mod) % mod);}
-    friend modint operator-=(modint &x,modint y){return (((x() -= y()) %= mod) += mod) %= mod;}
-    friend modint operator*(modint x,modint y){return (x() * y() % mod);}
-    friend modint operator*=(modint &x,modint y){return (x() *= y()) %= mod;}
-};
-struct mat
-{
-    modint a[2][2];
-    mat(int x = 0)
-    {
-        memset(a,0,sizeof a);
-        a[0][0] = a[1][1] = x;
-    }
-    modint *operator[](int x){return a[x];}
-    friend mat operator*(mat x,mat y)
-    {
-        mat ret;
-        for (int i = 0; i < 2; i++)
-            for (int j = 0; j < 2; j++)
-                for (int k = 0; k < 2; k++)
-                    ret[i][j] += x[i][k] * y[k][j];
-        return ret;
-    }
-}base,origin;
-template<typename type>
-type binpow(type x,int y)
-{
-    type ret(1);
+    int x = a,y = b,tmp = 0;
     while (y)
     {
         if (y & 1)
-            ret = ret * x;
-        x = x * x;
+            tmp = (tmp + x) % mod;
+        x = (x * 2) % mod;
         y >>= 1;
     }
-    return ret;
+    return tmp;
+}
+struct mat
+{
+    int a[2][2];
+}origin,base;
+int binpow(int a,int b)
+{
+    int tmp = 1;
+    while (b)
+    {
+        if (b & 1)
+            tmp = mul(tmp,a);
+        a = mul(a,a);
+        b >>= 1;
+    }
+    return tmp;
+}
+void mat_mul(mat &x,mat y)
+{
+    memset(temp,0,sizeof(temp));
+    for (int i = 0; i <= 1; i++)
+        for (int j = 0; j <= 1; j++)
+            for (int k = 0; k <= 1; k++)
+                temp[i][j] = (temp[i][j] + mul(x.a[i][k],y.a[k][j])) % mod;
+    memcpy(x.a,temp,sizeof(temp));
+}
+int fib(int a)
+{
+    while (a)
+    {
+        if (a & 1)
+            mat_mul(origin,base);
+        a >>= 1;
+        mat_mul(base,base);
+    }
+    return origin.a[0][0];
 }
 signed main()
 {
-    read(n),read(mod);
-    base[0][0] = base[0][1] = base[1][0] = 1;
-    origin[0][0] = origin[1][0] = 1;
-    origin = binpow(base,n + 1) * origin;
-    modint tmp = origin[0][0] - n - 2;
-    write(binpow(tmp,n + 1)());
+    origin.a[0][0] = origin.a[1][1] = 1;
+    base.a[0][1] = base.a[1][0] = base.a[0][0] = 1;
+    scanf("%lld%lld",&n,&mod);
+    printf("%lld",binpow(((fib(n + 2) - n - 2) % mod + mod) % mod,n + 1));
     return 0;
 }
