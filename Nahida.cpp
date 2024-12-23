fb3079b3f1406404a29f537185fec694d368266d
ybz2010 <ybz721@126.com>
2024-12-23 16:34:16 +0800

    P3597

--- Nahida.cpp
+++ Nahida.cpp
@@ -1,167 +1,66 @@
-#include <bits/stdc++.h>
-typedef long long LL;
+#include "iostream"
+#include "algorithm"
+#include "cstring"
+#include "cstdio"
 using namespace std;
-template <class T>
-inline void read(T &n)
-{
-	n = 0;
-	int ch = getchar(), f;
-	while ((ch < '0' || ch > '9') && ch != '-')
-		ch = getchar();
-	f = (ch == '-' ? ch = getchar(), -1 : 1);
-	while (ch >= '0' && ch <= '9')
-		n = n * 10 + (ch ^ 48), ch = getchar();
-	n *= f;
-}
-int const maxn = 4e5 + 5;
-int b[maxn], a[maxn];
-int f[maxn][21];
-int child[maxn][2], stk[maxn], val[maxn];
-int dep[maxn];
-int LOG[maxn];
-int Mx[maxn * 4], tag[maxn * 4];
+#define MAXN 125
+#define P 1000000000
+typedef long long ll;
+ll n, t, k;
 
-void Dfs(int now, int d)
+struct matrx
 {
-	dep[now] = d;
-	if (child[now][0])
-		Dfs(child[now][0], d + 1);
-	if (child[now][1])
-		Dfs(child[now][1], d + 1);
-}
-inline int GetMin(int tl, int tr)
+    ll a[MAXN][MAXN];
+} G[70], tmp, cur;
+
+bool work(matrx &a)
 {
-	int k = LOG[tr - tl + 1];
-	return min(f[tl][k], f[tr - (1 << k) + 1][k]);
+    ll p = 0;
+    for (ll i = 1; i <= n; ++i)
+        if ((k <= (p += a.a[i][0] - 1)))
+            return true;
+    return false;
 }
 
-#define lc (x << 1)
-#define rc (x << 1 | 1)
-inline void Update(int x) { Mx[x] = max(Mx[lc], Mx[rc]); }
-inline void Add(int x, int num) { Mx[x] += num, tag[x] += num; }
-inline void Pushdown(int x)
+void mul(matrx &x, matrx &y)
 {
-	if (tag[x])
-		Add(lc, tag[x]), Add(rc, tag[x]), tag[x] = 0;
+    for (ll i = 0; i <= 3 * n; ++i)
+        for (ll j = 0; j <= 3 * n; ++j)
+        {
+            tmp.a[i][j] = 0;
+            for (ll p = 0; p <= 3 * n; ++p)
+                tmp.a[i][j] += x.a[i][p] * y.a[p][j];
+        }
 }
-void upd(int x, int tl, int tr, int i, int j, int num)
-{
-	if (i <= tl && tr <= j)
-		return Add(x, num);
-	Pushdown(x);
-	int mid = (tl + tr) >> 1;
-	if (i <= mid)
-		upd(lc, tl, mid, i, j, num);
-	if (j > mid)
-		upd(rc, mid + 1, tr, i, j, num);
-	Update(x);
-}
-int que(int x, int tl, int tr, int i, int j)
-{
-	if (i <= tl && tr <= j)
-		return Mx[x];
-	Pushdown(x);
-	int mid = (tl + tr) >> 1, re = -1;
-	if (i <= mid)
-		re = max(re, que(lc, tl, mid, i, j));
-	if (j > mid)
-		re = max(re, que(rc, mid + 1, tr, i, j));
-	return re;
-}
-#undef lc
-#undef rc
 
 int main()
 {
-	int n;
-	read(n);
-	LOG[1] = 0;
-	for (int i = 2; i <= n + n; ++i)
-		LOG[i] = LOG[i >> 1] + 1;
-	for (int i = 1; i <= n; ++i)
-		read(a[i]);
-	for (int i = 1; i <= n; ++i)
-		if (a[i] == 1)
-		{
-			for (int j = 1; j <= n; ++j, i = i % n + 1)
-				b[j] = a[i];
-			break;
-		}
-	for (int i = n + 1; i <= n + n; ++i)
-		b[i] = b[i - n];
-	for (int i = 1; i <= n + n; ++i)
-		f[i][0] = b[i];
-	for (int k = 1; k <= 19; ++k)
-		for (int i = 1; i + (1 << k) - 1 <= n + n; ++i)
-			f[i][k] = min(f[i][k - 1], f[i + (1 << (k - 1))][k - 1]);
-	/* 构造初始笛卡尔树 */
-
-	int l = n + 1, r = n + n, top = 0;
-	for (int i = l; i <= r; ++i)
-	{
-		int x = b[i];
-		while (top && val[top] > x)
-			--top;
-		if (top)
-			child[stk[top]][1] = i;
-		child[i][0] = stk[top + 1];
-		stk[++top] = i;
-		val[top] = x;
-		stk[top + 1] = 0;
-	}
-
-	Dfs(n + 1, 1);
-
-	for (int i = n + 1; i <= n + n; i++)
-		cout << dep[i] << " ";
-	for (int i = l; i <= r; ++i)
-		upd(1, 1, n + n, i, i, dep[i]);
-	int tmp = que(1, 1, n + n, l, r);
-	int ans = 0x7f7f7f7f, pos = 0;
-	if (tmp < ans)
-		ans = tmp, pos = l;
-	
-	for (int i = n; i > 1; --i)
-	{
-
-		/* 删掉最后一个 */
-		int del = i + n;
-		int tl = l, tr = r - 1, mid, tans;
-		while (tl <= tr)
-		{
-			mid = (tl + tr) >> 1;
-			if (GetMin(mid, r) < b[del])
-				tans = mid, tl = mid + 1;
-			else
-				tr = mid - 1;
-		}
-		upd(1, 1, n + n, tans + 1, del, -1);
-
-		/* 把刚刚删掉的插到第一个 */
-
-		int ins = i;
-		tl = l, tr = r - 1;
-		while (tl <= tr)
-		{
-			mid = (tl + tr) >> 1;
-			if (GetMin(l, mid) < b[ins])
-				tans = mid, tr = mid - 1;
-			else
-				tl = mid + 1;
-		}
-		int d = que(1, 1, n + n, tans, tans);
-		upd(1, 1, n + n, ins, ins, d);
-		upd(1, 1, n + n, ins, tans - 1, 1);
-
-		--l, --r;
-		int tmp = que(1, 1, n + n, l, r);
-		if (tmp < ans)
-			ans = tmp, pos = l;
-	}
-	int k = b[pos];
-	for (int i = 1; i <= n; ++i)
-		if (k == a[i])
-			printf("%d %d\n", ans, i - 1);
-
-	return 0;
+    ll m;
+    cin >> n >> m >> k;
+    G[0].a[0][0] = 1;
+    for (ll i = 1; i <= n; ++i)
+        cur.a[i][i] = G[0].a[i][0] = G[0].a[i][i + n] = G[0].a[i + n][i + 2 * n] = 1;
+    for (ll i = 0, u, v, w; i < m; ++i)
+    {
+        scanf("%lld%lld%lld", &u, &v, &w);
+        ++G[0].a[u + (w - 1) * n][v];
+    }
+    ll i;
+    for (i = 1;; ++i)
+    {
+        if (i > 65)
+            return puts("-1"), 0;
+        mul(G[i - 1], G[i - 1]), G[i] = tmp;
+        if (work(G[i]))
+            break;
+    }
+    long long ans = 0;
+    for (i--; ~i; --i)
+    {
+        mul(cur, G[i]);
+        if (!work(tmp))
+            cur = tmp, ans += (1ll << i);
+    }
+    cout << ans;
 }
+// qwq
