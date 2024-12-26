3c1d55bf0d49a72c959d0e3f08bf0f1169c7cfb9
ybz2010 <ybz721@126.com>
2024-12-26 18:54:13 +0800

    2024-12-26 T3

--- Klee.cpp
+++ Klee.cpp
@@ -1,9 +1,55 @@
-# OI
-
-#### 介绍
-
-什么也没有
-
-就是存代码的仓库
-
-但是 `contest` 文件夹里的东西不要看，有些比赛是不允许公开代码的。
+#include <bits/stdc++.h>
+using namespace std;
+int read()
+{
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < 48 || ch > 57)
+    {
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    while (ch >= 48 && ch <= 57)
+        x = x * 10 + ch - 48, ch = getchar();
+    return x * f;
+}
+int s[200001];
+int sj[3001][3001];
+int md(const int a)
+{
+    return a < 998244353 ? a : a - 998244353;
+}
+int main()
+{
+    int a, b(0);
+    long long d(1);
+    a = read();
+    for (int v = 1; v < a; ++v)
+    {
+        s[v] = read();
+        b = max(b, s[v]);
+    }
+    for (int v = 0; v <= b; ++v)
+        sj[v][0] = 1;
+    sj[1][1] = 2;
+    for (int v = 2; v <= b; ++v)
+    {
+        sj[v][1] = v + 1;
+        sj[v][v] = v + 1;
+        for (int var = 2; var < v; ++var)
+            sj[v][var] = md(sj[v - 1][var] + sj[v - 1][var - 1]);
+    }
+    for (int i = 1; i <= b; i++)
+    {
+        for (int j = 1; j < i; j++)
+            cout << sj[i][j] << " ";
+        cout << endl;
+    }
+        
+    for (int v = a; v; --v)
+        if (s[v - 1] <= s[v])
+            d = d * sj[s[v]][s[v - 1]] % 998244353;
+    cout << d;
+    return 0;
+}
--- exam/2024-12-26/C/sort.cpp
+++ exam/2024-12-26/C/sort.cpp
@@ -1,38 +1,44 @@
-#include <bits/extc++.h>
+#include<bits/extc++.h>
 #define int long long
+#define Inv(x) binpow(x,mod - 2)
 using namespace std;
 const int maxn = 2e5 + 5;
+const int mod = 998244353;
 int n;
-int p[maxn];
-vector<int> ec;
+int c[maxn];
+int fac[maxn],inv[maxn];
+int binpow(int x,int y)
+{
+    int ret = 1;
+    while (y)
+    {
+        if (y & 1)
+            ret = ret * x % mod;
+        x = x * x % mod;
+        y >>= 1;
+    }
+    return ret;
+}
+void init()
+{
+    fac[0] = inv[0] = 1; 
+    for (int i = 1; i <= 2e5; i++)
+    {
+        fac[i] = fac[i - 1] * i % mod;
+        inv[i] = Inv(fac[i]);
+    }
+}
+int C(int n,int m){return n ++,n < m || n < 0 || m < 0 ? 0 : fac[n] * inv[m] % mod * inv[n - m] % mod;}
 signed main()
 {
+    init();
     scanf("%lld",&n);
-    ec.resize(n + 5,0);
     for (int i = 1; i < n; i++)
-        scanf("%lld",&ec[i]);
-    for (int i = 1; i <= n; i++)
-        p[i] = i;
-    int cnt = 0;
-    do
-    {
-        for (int i = 1; i <= n; i++)
-        vector<int> c(n + 5,0);
-        for (int i = 1; i < n; i++)
-        {
-            c[i] = 0;
-            for (int j = i + 1; j <= n; j++)
-            {
-                if (p[i] > p[j])
-                {
-                    swap(p[i],p[j]);
-                    c[i]++;
-                }
-            }
-        }
-        if (c == ec)
-            cnt ++;
-    } while (next_permutation(p + 1,p + n + 1));
-    cout << cnt;
+        cin >> c[i];
+    int ans = 1;
+    for (int i = n; i; i--)
+        if (c[i - 1] <= c[i])
+            ans = ans * C(c[i],c[i - 1]) % mod;
+    cout << ans;
     return 0;
 }
