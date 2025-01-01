13776d6bd9da5be47384eb7a32aa6b20d2739d19
ybz2010 <ybz721@126.com>
2025-01-01 17:16:59 +0800

    CF1400G

--- Nahida.cpp
+++ Nahida.cpp
@@ -1,75 +1,91 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fi			first
-#define se			second
-#define pb			push_back
-#define fz(i,a,b)	for(int i=a;i<=b;i++)
-#define fd(i,a,b)	for(int i=a;i>=b;i--)
-#define foreach(it,v) for(__typeof(v.begin()) it=v.begin();it!=v.end();it++)
-#define all(a)		a.begin(),a.end()
-#define fill0(a)	memset(a,0,sizeof(a))
-#define fill1(a)	memset(a,-1,sizeof(a))
-#define fillbig(a)	memset(a,0x3f,sizeof(a))
-#define y1			y1010101010101
-#define y0			y0101010101010
+#define fz(i, a, b) for (int i = a; i <= b; i++)
 #define int long long
-typedef pair<int,int> pii;
-inline int read(){
-	int x=0,neg=1;char c=getchar();
-	while(!isdigit(c)){
-		if(c=='-') neg=-1;
-		c=getchar();
+typedef pair<int, int> pii;
+inline int read()
+{
+	int x = 0, neg = 1;
+	char c = getchar();
+	while (!isdigit(c))
+	{
+		if (c == '-')
+			neg = -1;
+		c = getchar();
 	}
-	while(isdigit(c)) x=x*10+c-'0',c=getchar();
-	return x*neg;
+	while (isdigit(c))
+		x = x * 10 + c - '0', c = getchar();
+	return x * neg;
 }
-int n=read(),m=read();
-int cf[300005],l[300005],r[300005],cnt[300005],a[25],b[25];
-const int MOD=998244353;
-int fac[300005],inv[300005];
-int qpow(int x,int e){
-	int ans=1;while(e){if(e&1) ans=ans*x%MOD;x=x*x%MOD;e>>=1;} return ans;
+int n,m;
+int val[300005], l[300005], r[300005], a[25], b[25];
+const int mod = 998244353;
+int fac[300005], inv[300005];
+int binpow(int x,int y)
+{
+    int ret = 1;
+    while (y)
+    {
+        if (y & 1)
+            ret = ret * x % mod;
+        x = x * x % mod;
+        y >>= 1;
+    }
+    return ret;
 }
-inline void prework(){
-	fac[0]=1;for(int i=1;i<=300000;i++) fac[i]=fac[i-1]*i%MOD;
-	for(int i=0;i<=300000;i++) inv[i]=qpow(fac[i],MOD-2);
+inline void init()
+{
+	fac[0] = 1;
+    for (int i = 1; i <= 3e5; i++)
+        fac[i] = fac[i - 1] * i % mod;
+    inv[(int)3e5] = binpow(fac[(int)3e5],mod - 2);
+    for (int i = 3e5 - 1; ~i; i--)
+        inv[i] = inv[i + 1] * (i + 1) % mod;
 }
-int sum[300005][45];
-inline int getc(int x,int y){
-	if(x<y||x<0||y<0) return 0;
-	return fac[x]*inv[y]%MOD*inv[x-y]%MOD;
-}
-signed main(){
-	prework();
-	fz(i,1,n){
-		l[i]=read(),r[i]=read();
-		cf[l[i]]++;cf[r[i]+1]--;
+int f[300005][45];
+int C(int n,int m){return n < 0 || m < 0 || n < m ? 0 : fac[n] * inv[m] % mod * inv[n - m] % mod;}
+signed main()
+{
+	init();
+	n = read(), m = read();
+	fz(i, 1, n)
+	{
+		l[i] = read(), r[i] = read();
+		val[l[i]]++;
+		val[r[i] + 1]--;
 	}
-	int num=0;
-	fz(i,1,n){num+=cf[i];cnt[i]=num;}
-	fz(i,1,m) a[i]=read(),b[i]=read();
-	fz(i,0,40){
-		fz(j,1,n){
-			sum[j][i]=(sum[j-1][i]+getc(cnt[j]-i,j-i))%MOD;
-		}
-	}
-//	cout<<sum[3][1]<<" "<<sum[1][1]<<endl;
-	int ans=0;
-	for(int i=0;i<(1<<m);i++){
-		int _l=1,_r=n;
+	fz(i, 1, n)
+		val[i] += val[i - 1];
+	fz(i, 1, m) a[i] = read(), b[i] = read();
+	fz(j, 0, 30)
+		fz(i, 1, n)
+			f[i][j] = (f[i - 1][j] + C(val[i] - j, i - j)) % mod;
+	int ans = 0;
+	for (int k = 0; k < (1 << m); k++)
+	{
+		int tl = 1, tr = n;
 		set<int> st;
-		fz(j,1,m){
-			if(i>>(j-1)&1){
-				_l=max(_l,l[a[j]]);_r=min(_r,r[a[j]]);st.insert(a[j]);
-				_l=max(_l,l[b[j]]);_r=min(_r,r[b[j]]);st.insert(b[j]);
+		fz(i, 1, m)
+		{
+			if ((1 << (i - 1)) & k)
+			{
+				tl = max(tl, l[a[i]]);
+				tr = min(tr, r[a[i]]);
+				st.insert(a[i]);
+				tl = max(tl, l[b[i]]);
+				tr = min(tr, r[b[i]]);
+				st.insert(b[i]);
 			}
 		}
-		if(_l>_r) continue;
-		int calc=(sum[_r][st.size()]-sum[_l-1][st.size()]+MOD)%MOD;
-		int cnt1=__builtin_popcount(i);
-		if(cnt1&1) ans=(ans-calc+MOD)%MOD;
-		else ans=(ans+calc)%MOD;
+		if (tl > tr)
+			continue;
+		int calc = (f[tr][st.size()] - f[tl - 1][st.size()] + mod) % mod;
+		int cnt1 = __builtin_popcount(k);
+		if (cnt1 & 1)
+			ans = (ans - calc + mod) % mod;
+		else
+			ans = (ans + calc) % mod;
 	}
-	cout<<ans<<endl;
+	cout << ans << endl;
 	return 0;
 }
