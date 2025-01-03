112ace8d894d10c216bb1632d664389eeed219b9
ybz2010 <ybz721@126.com>
2025-01-03 15:43:13 +0800

    2025-01-03 T2

--- Nahida.cpp
+++ Nahida.cpp
@@ -1,102 +1,77 @@
 #include <bits/stdc++.h>
-using namespace std;
+#define int long long
 
-void read(int &x)
+using namespace std;
+inline int read()
 {
-	x = 0;
+	int res = 0, f = 1;
 	char ch = getchar();
 	while (!isdigit(ch))
 		ch = getchar();
 	while (isdigit(ch))
-		x = x * 10 + (ch & 15), ch = getchar();
+		res = (res << 1) + (res << 3) + (ch ^ 48), ch = getchar();
+	return res * f;
+}
+const int maxn = 5e5 + 10;
+int n, v[maxn << 1], sum[maxn << 1];
+struct Ayaka
+{
+	int l, r, val;
+} seg[maxn << 4];
+
+inline void pushup(int rt) { seg[rt].val = min(seg[rt << 1].val, seg[rt << 1 | 1].val); }
+
+void Build(int l, int r, int rt)
+{
+	seg[rt].l = l, seg[rt].r = r;
+	if (l == r)
+		return;
+	int mid = l + r >> 1;
+	Build(l, mid, rt << 1), Build(mid + 1, r, rt << 1 | 1);
+	pushup(rt);
 }
 
-int a, b, num[15];
-
-struct long_int
+void update(int rt, int pos, int k)
 {
-	short d[2050];
-	int len;
-    long_int(){memset(d,0,sizeof d);len = 0;}
-    short &operator[](int x){return d[x];}
-	long_int operator*(const long_int &rhs) const
+	int l = seg[rt].l, r = seg[rt].r;
+	if (l == r)
 	{
-		long_int res;
-		res.len = rhs.len + len - 1;
-		for (int i = 1; i <= len; i++)
-			for (int j = 1; j <= rhs.len; j++)
-			{
-				res.d[i + j - 1] += d[i] * rhs.d[j];
-			}
-		for (int i = 1; i <= res.len; i++)
-		{
-			res.d[i + 1] += res.d[i] / 10, res.d[i] %= 10;
-			if (i == res.len && res.d[i + 1])
-				res.len++;
-		}
-		return res;
+		seg[rt].val = k;
+		return;
 	}
-	bool operator<(const long_int &rhs) const
-	{
-		if (len != rhs.len)
-			return len < rhs.len;
-		for (int i = len; i >= 1; i--)
-			if (d[i] != rhs.d[i])
-				return d[i] < rhs.d[i];
-		return false;
-	}
-	void write()
-	{
-		for (int i = len; i >= 1; i--)
-			printf("%d", d[i]);
-		puts("");
-	}
-} x, y, ans1, ans2;
+	int mid = l + r >> 1;
+	if (pos <= mid)
+		update(rt << 1, pos, k);
+	else
+		update(rt << 1 | 1, pos, k);
+	pushup(rt);
+}
 
-void solve()
+int query(int ql, int qr, int rt)
 {
-	read(a), read(b);
-	if (a > b)
-		swap(a, b);
-	x.len = a, y.len = b;
-	for (int i = 1; i <= 9; i++)
-		read(num[i]);
-	for (int i = 9; i >= 1; i--)
-		while (num[i]--)
-		{
-			if (!a)
-				y.d[b--] = i;
-			else if (!b)
-				x.d[a--] = i;
-			else if (a == x.len && b == y.len)
-				x.d[a--] = i;
-			else if (b == y.len)
-				y.d[b--] = i;
-			else
-			{
-				x.d[a--] = i;
-				ans1 = x * y;
-				x.d[++a] = 0;
-				y.d[b--] = i;
-				ans2 = x * y;
-				y.d[++b] = 0;
-				if (ans1 < ans2)
-					y.d[b--] = i;
-				else
-					x.d[a--] = i;
-			}
-		}
-	// x.write();
-	// y.write();
-	(x * y).write();
+	int l = seg[rt].l, r = seg[rt].r;
+	if (ql <= l && qr >= r)
+		return seg[rt].val;
+	int mid = l + r >> 1, tmpVal = 1e9;
+	if (ql <= mid)
+		tmpVal = min(tmpVal, query(ql, qr, rt << 1));
+	if (qr > mid)
+		tmpVal = min(tmpVal, query(ql, qr, rt << 1 | 1));
+	return tmpVal;
 }
 
 signed main()
 {
-	//	freopen ("math.txt", "w", stdout);
-	int t;
-	read(t);
-	while (t--)
-		solve();
+	n = read(), Build(1, n << 1, 1);
+	for (int i = 1; i <= n; i++)
+		v[i] = read(), v[i + n] = v[i];
+	for (int i = 1; i <= (n << 1); i++)
+		sum[i] = sum[i - 1] + v[i];
+	int len = (n + 1) >> 1, Answer = 0;
+	for (int i = 1; i <= (n << 1); i++)
+		update(1, i, sum[i + len - 1] - sum[i - 1]);
+	for (int i = 1; i <= (n << 1); i++)
+		Answer = max(Answer, query(i, i + len - 1, 1));
+	printf("%lld\n", Answer);
 	return 0;
 }
